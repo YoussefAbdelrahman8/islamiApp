3beecdaf064d60a132e0b45587946c92ef437db8
Youssef Ashraf Othman Abdelrahman <youssef.abdelrahman158@gmail.com>
2024-10-29 17:36:09 +0300

    + AppStyle file + add some of darkTheme colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:islamiapp/AppStyle/AppStyle.dart';
 import 'package:islamiapp/Screens/AhadethDetailsScreen/AhadethDetailsScreen.dart';
 import 'package:islamiapp/Screens/HomeScreen/HomeScreen.dart';
 import 'package:islamiapp/Screens/QuraanDetailScreen/QuraanDetailsScreen.dart';
@@ -14,41 +15,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        iconTheme: const IconThemeData(
-          color: Color(0xffB7935F),
-        ),
-          appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.transparent,
-            titleTextStyle: TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.w700,
-              fontSize: 30,
-            ),
-          ),
-          scaffoldBackgroundColor: Colors.transparent,
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-              backgroundColor: Color(0xffB7935F),
-              unselectedIconTheme: IconThemeData(
-                color: Colors.white,
-                size: 35,
-              ),
-              selectedIconTheme: IconThemeData(
-                color: Colors.black,
-                size: 35,
-              ),
-              selectedItemColor: Colors.black,
-              unselectedItemColor: Colors.white),
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.deepPurple,
-              primary: const Color(0xffB7935F),
-              secondary: const Color(0xffB7935F).withOpacity(0.57),
-              onPrimary: Colors.white,
-              onSecondary: Colors.black),
-          dividerTheme: const DividerThemeData(
-            color: Color(0xffB7935F),
-            thickness: 2,
-          )),
+      theme: AppStyle.lightTheme,
+      darkTheme: AppStyle.darkTheme,
+      themeMode: ThemeMode.system,
       routes: {
         HomeScreen.routeName: (context) => const HomeScreen(),
         SplashScreen.routeName: (context) => const SplashScreen(),
