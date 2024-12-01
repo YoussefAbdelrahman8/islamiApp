b476c7e4ef8607925e2f546f54a76c2115f78c3b
Youssef Ashraf Othman Abdelrahman <youssef.abdelrahman158@gmail.com>
2024-12-01 15:29:42 +0200

    + Shared pref

--- lib/Providers/SettingsProvider.dart
+++ lib/Providers/SettingsProvider.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:islamiapp/SharedPrefService.dart';
 
 
 class SettingsProvider extends ChangeNotifier {
--- lib/SharedPrefService.dart
+++ lib/SharedPrefService.dart
@@ -1,27 +1,27 @@
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-//
-// class SharedPrefService {
-//   static const String themeKey = "theme";
-//   static const String localeKey = "locale";
-//
-//   static Future<void> saveTheme(String theme) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setString(themeKey, theme);
-//   }
-//
-//   static Future<String?> getTheme() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getString(themeKey);
-//   }
-//
-//   static Future<void> saveLocale(String locale) async {
-//     final prefs = await SharedPreferences.getInstance();
-//     await prefs.setString(localeKey, locale);
-//   }
-//
-//   static Future<String?> getLocale() async {
-//     final prefs = await SharedPreferences.getInstance();
-//     return prefs.getString(localeKey);
-//   }
-// }
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPrefService {
+  static const String themeKey = "theme";
+  static const String localeKey = "locale";
+
+  static Future<void> saveTheme(String theme) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(themeKey, theme);
+  }
+
+  static Future<String?> getTheme() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(themeKey);
+  }
+
+  static Future<void> saveLocale(String locale) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(localeKey, locale);
+  }
+
+  static Future<String?> getLocale() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString(localeKey);
+  }
+}
